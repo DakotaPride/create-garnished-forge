666c5be424101f627fc4460138f602e072799bf5
DakotaPride <rjtube13@gmail.com>
2023-07-03 12:55:42 -0500

    Port fabric

--- src/main/java/net/dakotapride/garnished/item/MeltedCinderFlourNutFoodItem.java
+++ src/main/java/net/dakotapride/garnished/item/MeltedCinderFlourNutFoodItem.java
@@ -1,7 +1,13 @@
 package net.dakotapride.garnished.item;
 
 import net.dakotapride.garnished.registry.GarnishedFoods;
+import net.dakotapride.garnished.registry.GarnishedItems;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -19,5 +25,36 @@ public class MeltedCinderFlourNutFoodItem extends Item implements IGarnishedItem
 	@Override
 	public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> tooltip, @NotNull TooltipFlag isAdvanced) {
 		tooltip.add(Component.translatable(meltedCinderFlourText()).withStyle(getCinderFlourColouring()));
+		if (getEffect() != null) {
+			tooltip.add(Component.translatable("text.garnished.nut.cinder_flour.effect",
+					getEffect().getDisplayName()).withStyle(getStandardColouring()));
+		}
+	}
+
+	public MobEffect getEffect() {
+		MobEffect effect = null;
+		Item item = this.getDefaultInstance().getItem();
+
+		if (item == GarnishedItems.SPEED_CINDER_CASHEW.get()) {
+			effect = MobEffects.MOVEMENT_SPEED;
+		} else if (item == GarnishedItems.STRENGTH_CINDER_WALNUT.get()) {
+			effect = MobEffects.DAMAGE_BOOST;
+		} else if (item == GarnishedItems.HASTE_CINDER_ALMOND.get()) {
+			effect = MobEffects.DIG_SPEED;
+		} else if (item == GarnishedItems.RESISTANCE_CINDER_PECAN.get()) {
+			effect = MobEffects.DAMAGE_RESISTANCE;
+		} else if (item == GarnishedItems.NIGHT_VISION_CINDER_PISTACHIO.get()) {
+			effect = MobEffects.NIGHT_VISION;
+		} else if (item == GarnishedItems.FIRE_RESISTANCE_CINDER_MACADAMIA.get()) {
+			effect = MobEffects.FIRE_RESISTANCE;
+		}
+
+		return effect;
+	}
+
+	@Override
+	public @Nullable FoodProperties getFoodProperties(ItemStack stack, @Nullable LivingEntity entity) {
+		return new FoodProperties.Builder().saturationMod(GarnishedFoods.MELTED_CINDER_FLOUR_NUT.getSaturationModifier())
+				.nutrition(GarnishedFoods.MELTED_CINDER_FLOUR_NUT.getNutrition()).effect(new MobEffectInstance(getEffect()), 1.0F).build();
 	}
 }
