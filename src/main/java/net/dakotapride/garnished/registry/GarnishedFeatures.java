8ddb9673d7ffce182aa4711139958cc126f41b57
DakotaPride <rjtube13@gmail.com>
2023-09-08 21:52:11 -0500

    v0.7.2 - fix sapling generation

--- src/main/java/net/dakotapride/garnished/registry/GarnishedFeatures.java
+++ src/main/java/net/dakotapride/garnished/registry/GarnishedFeatures.java
@@ -63,10 +63,10 @@ public class GarnishedFeatures {
     public static final RegistryObject<ConfiguredFeature<?, ?>> BUHG_TREE_CONFIGURED = CONFIGURED_FEATURES.register("peanut_tree_configured",
             () -> new ConfiguredFeature<>(Feature.TREE,
                     new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.OAK_LOG),
-                            new StraightTrunkPlacer(4, 2, 0),
+                            new StraightTrunkPlacer(1, 1, 0),
                             new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(Blocks.OAK_LEAVES.defaultBlockState(), 3)
                                     .add(GarnishedBlocks.BUHG_LEAVES.get().defaultBlockState(), 1)),
-                            new RandomSpreadFoliagePlacer(ConstantInt.of(3), ConstantInt.of(0), ConstantInt.of(2), 30),
+                            new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 3),
                             new TwoLayersFeatureSize(1, 0, 1)).dirt(BlockStateProvider.simple(Blocks.DIRT)).forceDirt().build()));
     public static final RegistryObject<ConfiguredFeature<?, ?>> WALNUT_TREE_CONFIGURED = CONFIGURED_FEATURES.register("walnut_tree_configured",
             () -> new ConfiguredFeature<>(Feature.TREE,
