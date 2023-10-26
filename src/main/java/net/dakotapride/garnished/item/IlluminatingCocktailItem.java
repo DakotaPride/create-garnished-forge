f6e503cb2acb5b5f8e2bcadfc49dfc31ce0bc027
DakotaPride <rjtube13@gmail.com>
2023-10-26 17:48:49 -0500

    v1.2

--- src/main/java/net/dakotapride/garnished/item/IlluminatingCocktailItem.java
+++ src/main/java/net/dakotapride/garnished/item/IlluminatingCocktailItem.java
@@ -4,20 +4,20 @@ import net.dakotapride.garnished.registry.GarnishedEffects;
 import net.dakotapride.garnished.registry.GarnishedFoods;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class IlluminatingCocktailItem extends Item implements IGarnishedItem {
+	private static final int DRINK_DURATION = 40;
 	public IlluminatingCocktailItem(Properties properties) {
 		super(properties.food(GarnishedFoods.ILLUMINATING_COCKTAIL).stacksTo(16));
 	}
@@ -44,6 +44,26 @@ public class IlluminatingCocktailItem extends Item implements IGarnishedItem {
 			return stack;
 		}
 
+	}
+
+	@Override
+	public int getUseDuration(@NotNull ItemStack stack) {
+		return DRINK_DURATION;
+	}
+
+	@Override
+	public @NotNull UseAnim getUseAnimation(ItemStack stack) {
+		return UseAnim.DRINK;
+	}
+
+	@Override
+	public @NotNull SoundEvent getDrinkingSound() {
+		return SoundEvents.GENERIC_DRINK;
+	}
+
+	@Override
+	public @NotNull SoundEvent getEatingSound() {
+		return SoundEvents.GENERIC_DRINK;
 	}
 
 	@Override
