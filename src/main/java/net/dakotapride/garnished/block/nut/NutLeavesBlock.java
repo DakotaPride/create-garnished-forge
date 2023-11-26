76dca217c78d7917007a02d7ba6e649b491f325a
DakotaPride <rjtube13@gmail.com>
2023-11-25 18:48:58 -0600

    v1.3 - Sweet Succulent Sugary Treats

--- src/main/java/net/dakotapride/garnished/block/nut/NutLeavesBlock.java
+++ src/main/java/net/dakotapride/garnished/block/nut/NutLeavesBlock.java
@@ -1,12 +1,10 @@
 package net.dakotapride.garnished.block.nut;
 
-import io.github.fabricators_of_create.porting_lib.extensions.IShearable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
