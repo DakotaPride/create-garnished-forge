a92c0462b417c2505656bec6f4b347917d7be4b5
DakotaPride <rjtube13@gmail.com>
2024-03-15 13:57:20 -0500

    v1.7.1 - Minor tweaks and fixes

---
+++ src/main/java/net/dakotapride/garnished/mixin/PlayerMixin.java
@@ -0,0 +1,31 @@
+package net.dakotapride.garnished.mixin;
+
+import net.dakotapride.garnished.item.hatchet.HatchetToolItem;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin({Player.class})
+public abstract class PlayerMixin extends LivingEntity {
+	protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level world) {
+		super(entityType, world);
+	}
+
+	@Inject(method = "wasKilled", at = {@At("TAIL")})
+	private void killedEntity(ServerLevel level, LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+		ItemStack main = this.getMainHandItem();
+		Item var6 = main.getItem();
+		if (var6 instanceof HatchetToolItem item) {
+			item.dropsUponDeath(this, entity);
+		}
+
+	}
+}
