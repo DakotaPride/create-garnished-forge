a0c0fcc8dfe6e8893bb758bd3f4598d922d87b7a
Dakota <rjtube13@gmail.com>
2025-04-24 09:11:04 -0500

    0.1.3 - Implement basis of biome-based benefits

---
+++ src/main/java/net/dakotapride/creategarnished/event/ProvideEffectsFromConsumptionEvent.java
@@ -0,0 +1,74 @@
+package net.dakotapride.creategarnished.event;
+
+import net.dakotapride.creategarnished.CreateGarnished;
+import net.dakotapride.creategarnished.registry.CreateGarnishedTags;
+import net.minecraft.core.Holder;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.biome.Biome;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
+
+@EventBusSubscriber(modid = CreateGarnished.ID, bus = EventBusSubscriber.Bus.GAME)
+public class ProvideEffectsFromConsumptionEvent {
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void provideEffectsFromConsumingFoods(LivingEntityUseItemEvent.Finish event) {
+        ItemStack itemStack = event.getItem();
+        LivingEntity entity = event.getEntity();
+        Holder<Biome> biome = entity.level().getBiome(entity.blockPosition());
+
+        for (YoinkFromHereList list : YoinkFromHereList.values()) {
+            boolean isInBiome = biome.is(list.getBiomeTagKey());
+
+            if (itemStack.is(list.getItemTagKey()) && isInBiome) {
+                pullEffect(entity, list.getEffectHolder());
+            }
+
+        }
+    }
+
+    private static void pullEffect(LivingEntity entity, Holder<MobEffect> effectHolder) {
+        entity.addEffect(new MobEffectInstance(effectHolder, 120 * 20, 1, false, false, false));
+    }
+
+    enum YoinkFromHereList {
+        JUNGLE(CreateGarnishedTags.JUNGLE_SPECIAL_FOODS, BiomeTags.IS_JUNGLE, MobEffects.DIG_SPEED),
+        FLOWER_FOREST(CreateGarnishedTags.FLOWER_FOREST_SPECIAL_FOODS, CreateGarnishedTags.IS_ACCEPTED_FLOWER_BIOME, MobEffects.REGENERATION),
+        BIRCH_FOREST(CreateGarnishedTags.BIRCH_FOREST_SPECIAL_FOODS, CreateGarnishedTags.IS_BIRCH_FOREST, MobEffects.MOVEMENT_SPEED),
+        TAIGA(CreateGarnishedTags.TAIGA_SPECIAL_FOODS, BiomeTags.IS_TAIGA, MobEffects.INVISIBILITY),
+
+
+        ;
+
+        final TagKey<Item> itemTagKey;
+        final TagKey<Biome> biomeTagKey;
+        final Holder<MobEffect> effectHolder;
+
+        YoinkFromHereList(TagKey<Item> itemTagKey, TagKey<Biome> biomeTagKey, Holder<MobEffect> effectHolder) {
+            this.itemTagKey = itemTagKey;
+            this.biomeTagKey = biomeTagKey;
+            this.effectHolder = effectHolder;
+        }
+
+        public TagKey<Item> getItemTagKey() {
+            return itemTagKey;
+        }
+
+        public TagKey<Biome> getBiomeTagKey() {
+            return biomeTagKey;
+        }
+
+        public Holder<MobEffect> getEffectHolder() {
+            return effectHolder;
+        }
+    }
+}
--- src/main/java/net/dakotapride/creategarnished/registry/CreateGarnishedTags.java
+++ src/main/java/net/dakotapride/creategarnished/registry/CreateGarnishedTags.java
@@ -2,11 +2,23 @@ package net.dakotapride.creategarnished.registry;
 
 import net.dakotapride.creategarnished.CreateGarnished;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.biome.Biome;
 
 public class CreateGarnishedTags {
     public static TagKey<Item> PANCAKES = TagKey.create(BuiltInRegistries.ITEM.key(), CreateGarnished.asResource("pancakes"));
     public static TagKey<EntityType<?>> FLAPJACK_ADJACENT_ENTITY = TagKey.create(BuiltInRegistries.ENTITY_TYPE.key(), CreateGarnished.asResource("acceptable_birbs_for_flapjack"));
+
+    // Special foods
+    public static TagKey<Item> JUNGLE_SPECIAL_FOODS = TagKey.create(BuiltInRegistries.ITEM.key(), CreateGarnished.asResource("special_foods/jungle"));
+    public static TagKey<Item> TAIGA_SPECIAL_FOODS = TagKey.create(BuiltInRegistries.ITEM.key(), CreateGarnished.asResource("special_foods/taiga"));
+    public static TagKey<Item> FLOWER_FOREST_SPECIAL_FOODS = TagKey.create(BuiltInRegistries.ITEM.key(), CreateGarnished.asResource("special_foods/flower"));
+    public static TagKey<Item> BIRCH_FOREST_SPECIAL_FOODS = TagKey.create(BuiltInRegistries.ITEM.key(), CreateGarnished.asResource("special_foods/birch"));
+
+    public static TagKey<Biome> IS_ACCEPTED_FLOWER_BIOME = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("c", "is_flower_biome"));
+    public static TagKey<Biome> IS_BIRCH_FOREST = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("c", "is_birch_forest"));
 }
