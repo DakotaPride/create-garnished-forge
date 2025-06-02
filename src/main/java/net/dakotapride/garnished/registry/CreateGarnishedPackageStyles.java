153ba3061dc960b6f231a3d3f082c671a43bd753
Dakota <rjtube13@gmail.com>
2025-06-01 19:11:48 -0500

    v2.1.4

--- src/main/java/net/dakotapride/garnished/registry/CreateGarnishedPackageStyles.java
+++ src/main/java/net/dakotapride/garnished/registry/CreateGarnishedPackageStyles.java
@@ -6,6 +6,7 @@ import com.simibubi.create.content.logistics.box.PackageItem;
 import com.simibubi.create.content.logistics.box.PackageStyles;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.engine_room.flywheel.lib.model.baked.PartialModel;
+import net.dakotapride.garnished.CreateGarnished;
 import net.minecraft.resources.ResourceLocation;
 
 import static net.dakotapride.garnished.CreateGarnished.REGISTRATE;
@@ -24,7 +25,7 @@ public class CreateGarnishedPackageStyles {
 
     public static final ItemEntry<PackageItem> NUTS_PACKAGE = REGISTRATE
             .item("nuts_package", prop -> new PackageItem(prop,
-                    new PackageStyles.PackageStyle("nuts", 12, 12, 23, false)))
+                    new PackageStyles.PackageStyle("nuts", 12, 12, 23, true)))
             .properties(prop -> prop.stacksTo(1))
             .register();
 
@@ -34,6 +35,6 @@ public class CreateGarnishedPackageStyles {
 
     public static void registerPackageModel(ResourceLocation id, int width, int height) {
         AllPartialModels.PACKAGES.put(id, PartialModel.of(id.withPrefix("item/")));
-        //AllPartialModels.PACKAGE_RIGGING.put(id, PartialModel.of(Create.asResource("item/package/rigging_" + width + "x" + height)));
+        AllPartialModels.PACKAGE_RIGGING.put(id, PartialModel.of(id.withPrefix("item/rigging_")));
     }
 }
